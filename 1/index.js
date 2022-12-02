e490ab1824e1366f744aeba55b8ba6145a6981aa
Alexander Sandor <alexander.sandor@gmail.com>
2022-12-02 20:43:08 +0100

    Callback hell

--- 1/index.js
+++ 1/index.js
@@ -3,31 +3,30 @@ var fs = require('fs');
 console.log("starting ");
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    let splitData = data.split("\n");
+    let splitData = data
+        .split("\n")
+        .reduce((split, s) => {
+            if (s) {
+                split.at(-1).push(parseInt(s));
+            } else {
+                split.push([]);
+            }
 
-    let maxList = []; 
-    let currentValue = 0;
+            return split;
+        }, [[]])
+        .map((val) => {
+            return val
+                .reduce((sum, val) => {
+                    return sum + val;
+                }, 0)
+        })
+        .sort((a, b) => {
+            return b - a;
+        })
+        .slice(0, 3)
+        .reduce((sum, val) => {
+            return sum + val;
+        }, 0);
 
-    splitData.forEach((s) => {
-        if (s) {
-            currentValue += parseInt(s);
-        } else {
-            maxList.push(currentValue)
-            currentValue = 0;
-        }
-    })
-    maxList.push(currentValue)
-
-    maxList.sort((a, b) => {
-        return b - a;
-    });
-
-    let topThree = maxList.slice(0, 3);
-
-    console.log("Final ", topThree);
-    console.log("Sum", topThree.reduce((sum, current) => {
-        return sum += current;
-    }));
-
-
+    console.log(splitData);
 });
