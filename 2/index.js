8196bf8deca25d11eab444c90c00768b3b2c8612
Alexander Sandor <alexander.sandor@gmail.com>
2022-12-02 21:17:53 +0100

    Done with two

--- 2/index.js
+++ 2/index.js
@@ -1,15 +1,6 @@
 var fs = require('fs');
 
 console.log("starting ");
-
-// Lose = 0
-// Draw = 3
-// Win = 6
-
-// Rock = 1
-// Paper = 2
-// Scicor = 3
-
 const MatchResult = {
     Lose: 0,
     Draw: 3,
@@ -17,9 +8,6 @@ const MatchResult = {
 };
 
 const Types = {
-    // Rock: "Rock",
-    // Paper: "Paper",
-    // Scissor: "Scissor",
     Rock: 1,
     Paper: 2,
     Scissor: 3,
@@ -27,33 +15,33 @@ const Types = {
 
 function rockGame(opponent) {
     switch (opponent) {
-        case Types.Rock:
-            return MatchResult.Draw  + Types.Rock;
-        case Types.Paper:
-            return MatchResult.Win  + Types.Paper;
-        case Types.Scissor:
+        case MatchResult.Draw:
+            return MatchResult.Draw + Types.Rock;
+        case MatchResult.Win:
+            return MatchResult.Win + Types.Paper;
+        case MatchResult.Lose:
             return MatchResult.Lose + Types.Scissor;
     }
 }
 
 function paperGame(opponent) {
     switch (opponent) {
-        case Types.Rock:
+        case MatchResult.Lose:
             return MatchResult.Lose  + Types.Rock;
-        case Types.Paper:
+        case MatchResult.Draw:
             return MatchResult.Draw  + Types.Paper;
-        case Types.Scissor:
+        case MatchResult.Win:
             return MatchResult.Win + Types.Scissor;
     }
 }
 
 function scissorGame(opponent) {
     switch (opponent) {
-        case Types.Rock:
+        case MatchResult.Win:
             return MatchResult.Win  + Types.Rock;
-        case Types.Paper:
+        case MatchResult.Lose:
             return MatchResult.Lose  + Types.Paper;
-        case Types.Scissor:
+        case MatchResult.Draw:
             return MatchResult.Draw + Types.Scissor;
     }
 }
@@ -64,14 +52,17 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
             .map((key) => {
                 switch (key) {
                     case "A":
-                    case "X":
                         return Types.Rock;
                     case "B":
-                    case "Y":
                         return Types.Paper;
                     case "C":
-                    case "Z":
                         return Types.Scissor;
+                    case "X":
+                        return MatchResult.Lose;
+                    case "Y":
+                        return MatchResult.Draw;
+                    case "Z":
+                        return MatchResult.Win;
                 }
             })
     });
