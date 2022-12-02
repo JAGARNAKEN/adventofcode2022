4bfcf7a1324aac0849a43112f100f52ddfc7232e
Alexander Sandor <alexander.sandor@gmail.com>
2022-12-02 22:15:01 +0100

    Reduced solution a bit

--- 2/index.js
+++ 2/index.js
@@ -1,6 +1,19 @@
 var fs = require('fs');
 
 console.log("starting ");
+
+const ResultIndex = {
+    Draw: 0,
+    Win: 1,
+    Lose: 2,
+};
+
+const TypeIndex = {
+    Rock: 0,
+    Paper: 1,
+    Scissor: 2
+};
+
 const MatchResult = {
     Lose: 0,
     Draw: 3,
@@ -13,50 +26,36 @@ const Types = {
     Scissor: 3,
 };
 
-function rockGame(opponent) {
-    switch (opponent) {
-        case MatchResult.Draw:
-            return MatchResult.Draw + Types.Rock;
-        case MatchResult.Win:
-            return MatchResult.Win + Types.Paper;
-        case MatchResult.Lose:
-            return MatchResult.Lose + Types.Scissor;
-    }
+const typesList = [Types.Rock, Types.Paper, Types.Scissor];
+
+function typeAfterResult(typeIndex, resultIndex) {
+    const listIndex = (typeIndex + resultIndex) % typesList.length;
+    return typesList[listIndex];
 }
 
-function paperGame(opponent) {
-    switch (opponent) {
-        case MatchResult.Lose:
-            return MatchResult.Lose  + Types.Rock;
+function resultToIndex(result) {
+    switch (result) {
         case MatchResult.Draw:
-            return MatchResult.Draw  + Types.Paper;
-        case MatchResult.Win:
-            return MatchResult.Win + Types.Scissor;
-    }
-}
-
-function scissorGame(opponent) {
-    switch (opponent) {
-        case MatchResult.Win:
-            return MatchResult.Win  + Types.Rock;
+            return ResultIndex.Draw;
         case MatchResult.Lose:
-            return MatchResult.Lose  + Types.Paper;
-        case MatchResult.Draw:
-            return MatchResult.Draw + Types.Scissor;
+            return ResultIndex.Lose;
+        case MatchResult.Win:
+            return ResultIndex.Win;
     }
 }
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    let decodedGames = data.split("\n").map((val) => {
+    let decodedGames = data.split("\n")
+        .map((val) => {
         return val.split(" ")
             .map((key) => {
                 switch (key) {
                     case "A":
-                        return Types.Rock;
+                        return TypeIndex.Rock;
                     case "B":
-                        return Types.Paper;
+                        return TypeIndex.Paper;
                     case "C":
-                        return Types.Scissor;
+                        return TypeIndex.Scissor;
                     case "X":
                         return MatchResult.Lose;
                     case "Y":
@@ -65,17 +64,10 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
                         return MatchResult.Win;
                 }
             })
-    });
+        });
     
-    let gameResults = decodedGames.map((game) => {
-        switch (game[0]) {
-            case Types.Rock:
-                return rockGame(game[1]) 
-            case Types.Paper:
-                return paperGame(game[1]) 
-            case Types.Scissor:
-                return scissorGame(game[1]) 
-        }
+    let gameResults = decodedGames.map(([setup, result]) => {
+        return result + typeAfterResult(setup, resultToIndex(result));
     })
 
     let finalScore = gameResults.reduce((sum, val) => {
